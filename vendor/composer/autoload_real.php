3ad4f7d00dbc674fa4db180b13d4a0c629f2e3c6
nizaamomer <nizaamomer@gmail.com>
2024-08-14 21:18:58 +0300

    adding MIT License file for package Copyright

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit7ea4d0b3614c523450a0af075466941c
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit7ea4d0b3614c523450a0af075466941c', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit7ea4d0b3614c523450a0af075466941c', 'loadClassLoader'));
@@ -31,6 +33,25 @@ class ComposerAutoloaderInit7ea4d0b3614c523450a0af075466941c
 
         $loader->register(true);
 
+        $includeFiles = \Composer\Autoload\ComposerStaticInit7ea4d0b3614c523450a0af075466941c::$files;
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire7ea4d0b3614c523450a0af075466941c($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequire7ea4d0b3614c523450a0af075466941c($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
